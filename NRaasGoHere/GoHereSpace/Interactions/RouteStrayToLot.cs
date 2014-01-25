fccd062c02e43946ae3888df0c2e50a4cf09ff5e
Chain-Reaction <chain.reaction33@gmail.com>
2014-01-24 19:44:09 -0500

    1.67 ScriptCore, Stray fix, & another x86 fix

---
+++ NRaasGoHere/GoHereSpace/Interactions/RouteStrayToLot.cs
@@ -0,0 +1,53 @@
+﻿using NRaas.CommonSpace.Helpers;
+using Sims3.Gameplay;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Autonomy;
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.PetSystems;
+using Sims3.Gameplay.Socializing;
+using Sims3.SimIFace;
+using Sims3.UI;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRaas.GoHereSpace.Interactions
+{
+    public class StrayRouteToLot : Interaction<IActor, Lot>
+    {
+        public static InteractionDefinition Singleton = new Definition();
+
+        public override bool Run()
+        {
+            bool flag = false;            
+            if (base.Target.IsResidentialLot)
+            {
+                Door door = null;
+                flag = base.Target.RouteToFrontDoorOrMailbox((Sim) base.Actor, Sim.MinDistanceFromDoorWhenVisiting, Sim.MaxDistanceFromDoorWhenVisiting, ref door, true, true);
+            }
+            else
+            {
+                Route route = base.Actor.CreateRoute();
+                base.Target.PlanToLotEx(route);
+                flag = base.Actor.DoRoute(route);
+            }
+
+            return flag;
+        }
+
+        [DoesntRequireTuning]
+        private class Definition : MetaInteractionDefinition<Sim, Lot, StrayRouteToLot>
+        {
+            public Definition()
+            { }     
+
+            public override bool Test(Sim actor, Lot target, bool isAutonomous, ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+            {
+                return actor.SimDescription.IsStray;
+            }
+        }
+    }
+}
---
+++ NRaasGoHere/GoHereSpace/Interactions/StrayGoToLotEx.cs
@@ -0,0 +1,77 @@
+﻿using NRaas.CommonSpace.Helpers;
+using Sims3.Gameplay;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Autonomy;
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.PetSystems;
+using Sims3.SimIFace;
+using Sims3.UI;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRaas.GoHereSpace.Interactions
+{
+    class StrayGoToLotEx : StrayGoToLot, Common.IPreLoad, Common.IAddInteraction
+    {
+        static InteractionDefinition sOldSingleton;
+
+        public void OnPreLoad()
+        {     
+            sOldSingleton = Singleton as InteractionDefinition;
+            Singleton = new Definition();
+        }
+
+        public void AddInteraction(Common.InteractionInjectorList interactions)
+        {
+            interactions.Replace<Lot, StrayGoToLot.Definition>(Singleton as InteractionDefinition);
+        }
+
+        public void MakeStrayGoHomeEx(Sim sim, float x)
+        {
+            // still not sure why this fires immediately. My theory is it's something to do with
+            // changes in high LOD simulation or maybe instantiation is slow...
+            // either way, stop yanking the animal out of the world                       
+            this.OnGoToLotSuccessEx(sim, x);
+        }
+
+        public void OnGoToLotSuccessEx(Sim sim, float x)
+        {            
+            base.OnGoToLotSuccess(sim, x);
+        }
+
+        public override bool Run()
+        {           
+            StrayRouteToLot entry = StrayRouteToLot.Singleton.CreateInstanceWithCallbacks(base.Target, base.Actor, new InteractionPriority(InteractionPriorityLevel.CriticalNPCBehavior), false, false, null, new Callback(this.OnGoToLotSuccessEx), new Callback(this.MakeStrayGoHomeEx)) as StrayRouteToLot;
+            base.Actor.InteractionQueue.AddNext(entry);               
+
+            return true;
+        }
+
+        private new class Definition : StrayGoToLot.Definition
+        {
+            public Definition()
+            { }
+
+            public override InteractionInstance CreateInstance(ref InteractionInstanceParameters parameters)
+            {
+                InteractionInstance na = new StrayGoToLotEx();
+                na.Init(ref parameters);
+                return na;
+            }
+
+            public override string GetInteractionName(Sim actor, Lot target, InteractionObjectPair iop)
+            {
+                return base.GetInteractionName(actor, target, new InteractionObjectPair(sOldSingleton, target));
+            }            
+
+            public override bool Test(Sim actor, Lot target, bool isAutonomous, ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+            {
+                return actor.SimDescription.IsStray;
+            }
+        }
+    }
+}
