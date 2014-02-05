ff17a56128447640bdb596c6c87f74257f118170
JunJayMdM <JunJayMdM@outlook.com>
2014-02-05 23:30:32 +0100

    Overwatch update

--- NRaasOverwatch/VersionStamp.cs
+++ NRaasOverwatch/VersionStamp.cs
@@ -37,6 +37,11 @@ namespace NRaas
 
         /* Changes:
          *
+         * (JunJayMdM) Notifications about Stationary sims found during Stuck Check will only show if debugging is enabled
+         * 
+         * 
+         * 
+         * === V113 ===
          * (JunJayMdM) All alarms will be set to yield only when the game speed is Normal or Paused
          ** It might defeat the purpose of preventing the Alarm Manager from halting but that's not verified
          * 
@@ -49,6 +54,6 @@ namespace NRaas
          * Added "Settings \ Minimum Route Failure Testing Time"
          * 
          */
-        public static readonly int sVersion = 113;
+        public static readonly int sVersion = 114;
     }
 }
