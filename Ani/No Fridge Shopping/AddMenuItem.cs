51346fe20ca3b6a0ef698748a688da118ce41d56
icarus-allsorts <now_then_here_there@hotmail.com>
2015-02-14 04:31:19 +0900

    Ani No Fridge Shopping changes

--- Ani/No Fridge Shopping/AddMenuItem.cs
+++ Ani/No Fridge Shopping/AddMenuItem.cs
@@ -38,39 +38,39 @@ namespace ani_GroceryShopping
         // Methods
         static AddMenuItem()
         {
-            World.sOnWorldLoadFinishedEventHandler += new EventHandler(World_OnWorldLoadFinishedEventHandler);
-            World.sOnLotAddedEventHandler += new EventHandler(World_OnWorldLoadFinishedEventHandler);
+            World.OnWorldLoadFinishedEventHandler += new EventHandler(World_OnWorldLoadFinishedEventHandler);
+            World.OnLotAddedEventHandler += new EventHandler(World_OnWorldLoadFinishedEventHandler);
             World.OnObjectPlacedInLotEventHandler += new EventHandler(World_OnWorldLoadFinishedEventHandler);
         }
 
         static void World_OnWorldLoadFinishedEventHandler(object sender, EventArgs e)
         {
-            List<Fridge> fList = new List<Fridge>(Sims3.Gameplay.Queries.GetObjects<Fridge>());
-            foreach (Fridge f in fList)
+            //List<Fridge> fList = new List<Fridge>(Sims3.Gameplay.Queries.GetObjects<Fridge>());
+			foreach (Fridge f in Sims3.Gameplay.Queries.GetObjects<Fridge>())
             {
                 AddInteractions(f);
             }
 
-            List<Microwave> mList = new List<Microwave>(Sims3.Gameplay.Queries.GetObjects<Microwave>());
-            foreach (Microwave m in mList)
+            //List<Microwave> mList = new List<Microwave>(Sims3.Gameplay.Queries.GetObjects<Microwave>());
+			foreach (Microwave m in Sims3.Gameplay.Queries.GetObjects<Microwave>())
             {
                 AddInteractions(m);
             }
 
-            List<FoodProcessor> fpList = new List<FoodProcessor>(Sims3.Gameplay.Queries.GetObjects<FoodProcessor>());
-            foreach (FoodProcessor fp in fpList)
+            //List<FoodProcessor> fpList = new List<FoodProcessor>(Sims3.Gameplay.Queries.GetObjects<FoodProcessor>());
+			foreach (FoodProcessor fp in Sims3.Gameplay.Queries.GetObjects<FoodProcessor>())
             {
                 AddInteractions(fp);
             }
 
-            List<Stove> sList = new List<Stove>(Sims3.Gameplay.Queries.GetObjects<Stove>());
-            foreach (Stove s in sList)
+            //List<Stove> sList = new List<Stove>(Sims3.Gameplay.Queries.GetObjects<Stove>());
+			foreach (Stove s in Sims3.Gameplay.Queries.GetObjects<Stove>())
             {
                 AddInteractions(s);
             }
 
-            List<Grill> gList = new List<Grill>(Sims3.Gameplay.Queries.GetObjects<Grill>());
-            foreach (Grill g in gList)
+            //List<Grill> gList = new List<Grill>(Sims3.Gameplay.Queries.GetObjects<Grill>());
+			foreach (Grill g in Sims3.Gameplay.Queries.GetObjects<Grill>())
             {
                 AddInteractions(g);
             }
@@ -84,7 +84,7 @@ namespace ani_GroceryShopping
         {
             GameObject o = e.TargetObject as GameObject;
 
-            if (o != null)
+            //if (o != null)
                 AddInteractions(o);
             
             return ListenerAction.Keep;
@@ -92,57 +92,63 @@ namespace ani_GroceryShopping
 
         public static void AddInteractions(GameObject obj)
         {
-            Grill grill = obj as Grill;
-            Stove stove = obj as Stove;
-            Fridge fridge = obj as Fridge;
-            Microwave micro = obj as Microwave;            
-            FoodProcessor processor = obj as FoodProcessor;
+            //Grill grill = obj as Grill;
+            //Stove stove = obj as Stove;
+            //Fridge fridge = obj as Fridge;
+            //Microwave micro = obj as Microwave;            
+            //FoodProcessor processor = obj as FoodProcessor;
 
             if (obj != null && obj.Interactions != null)
             {
-                if (fridge != null)
+				if (obj is Fridge) //(fridge != null)
                 {
-                    InteractionObjectPair i2 = new InteractionObjectPair(OverridedFridge_Have.Singleton, obj);
-                    if (!obj.Interactions.Contains(i2))
-                    {
-                        obj.RemoveInteractionByType(Fridge_Have.Singleton);
+                    //InteractionObjectPair i2 = new InteractionObjectPair(OverridedFridge_Have.Singleton, obj);
+                    //if (!obj.Interactions.Contains(i2))
+                   // {
+						obj.RemoveInteractionByType(typeof(Fridge_Have.Definition));
+						obj.RemoveInteractionByType (typeof(Fridge_Prepare.PrepareDefinition));
                        // obj.Interactions.RemoveAt(0);
-                        obj.AddInteraction(OverridedFridge_Have.Singleton);
-                        obj.AddInteraction(OverridedFridge_Prepare.PrepareSingleton);
+                        obj.AddInteraction(OverridedFridge_Have.Singleton, true);
+                        obj.AddInteraction(OverridedFridge_Prepare.PrepareSingleton, true);
                         
-                    }
-                }else if(micro != null)
+                    //}
+                }
+				else if (obj is Microwave)//(micro != null)
                 {
-                    InteractionObjectPair i2 = new InteractionObjectPair(OverridedMicrowave_Have.Singleton, obj);
-                    if (!obj.Interactions.Contains(i2))
-                    {
-                        obj.AddInteraction(OverridedMicrowave_Have.Singleton);
+                    //InteractionObjectPair i2 = new InteractionObjectPair(OverridedMicrowave_Have.Singleton, obj);
+                    //if (!obj.Interactions.Contains(i2))
+                    //{
+						obj.RemoveInteractionByType (typeof(Microwave_Have.Definition));
+                        obj.AddInteraction(OverridedMicrowave_Have.Singleton, true);
 
-                    }
+                    //}
                 }
-                else if (processor != null)
+				else if (obj is FoodProcessor)//(processor != null)
                 {
-                    InteractionObjectPair i2 = new InteractionObjectPair(OverridedFoodProcessor_Have.Singleton, obj);
-                    if (!obj.Interactions.Contains(i2))
-                    {
-                        obj.AddInteraction(OverridedFoodProcessor_Have.Singleton);
-                    }
+                    //InteractionObjectPair i2 = new InteractionObjectPair(OverridedFoodProcessor_Have.Singleton, obj);
+                    //if (!obj.Interactions.Contains(i2))
+                    //{
+						obj.RemoveInteractionByType (typeof(FoodProcessor.FoodProcessor_Have.Definition));
+                        obj.AddInteraction(OverridedFoodProcessor_Have.Singleton, true);
+                    //}
                 }
-                else if (stove != null)
+				else if (obj is Stove)//(stove != null)
                 {
-                    InteractionObjectPair i2 = new InteractionObjectPair(OverridedStove_Have.Singleton, obj);
-                    if (!obj.Interactions.Contains(i2))
-                    {
-                        obj.AddInteraction(OverridedStove_Have.Singleton);
-                    }
+                    //InteractionObjectPair i2 = new InteractionObjectPair(OverridedStove_Have.Singleton, obj);
+                    //if (!obj.Interactions.Contains(i2))
+                    //{
+						obj.RemoveInteractionByType (typeof(Stove_Have.Definition));
+                        obj.AddInteraction(OverridedStove_Have.Singleton, true);
+                    //}
                 }
-                else if (grill != null)
+				else if (obj is Grill)//(grill != null)
                 {
-                    InteractionObjectPair i2 = new InteractionObjectPair(OverridedGrill_Have.Singleton, obj);
-                    if (!obj.Interactions.Contains(i2))
-                    {
-                        obj.AddInteraction(OverridedGrill_Have.Singleton);
-                    }
+                    //InteractionObjectPair i2 = new InteractionObjectPair(OverridedGrill_Have.Singleton, obj);
+                    //if (!obj.Interactions.Contains(i2))
+                    //{
+						obj.RemoveInteractionByType (typeof(Grill_Have.Definition));
+                        obj.AddInteraction(OverridedGrill_Have.Singleton, true);
+                    //}
                 }
 
             }
